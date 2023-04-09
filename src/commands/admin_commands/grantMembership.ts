3e5ba41fd4dead51c5177db4b5461693baea402f
josephjophy <3815hunters@gmail.com>
2023-04-09 18:23:33 -0500

    Stops already existing members from receiving a membership

--- src/commands/admin_commands/grantMembership.ts
+++ src/commands/admin_commands/grantMembership.ts
@@ -2,7 +2,7 @@ import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import { Command } from "../../interfaces/Command";
 import { createEmbeded } from "../../utils/embeded";
 import { commandLog, sendError } from "../../utils/logs";
-import { getContact, insertMembership } from "../../utils/supabase";
+import { getContact, isMember, insertMembership } from "../../utils/supabase";
 import {
   membershipCodeOptions,
   membershipLengthOptions,
@@ -36,12 +36,12 @@ export const grantmembership: Command = {
   run: async (interaction, client) => {
     await interaction.deferReply({ ephemeral: false });
     const { user } = interaction;
-
+    
     const discord_snowflake = interaction.options.get("user", true).user
       ?.id as string;
     const length = interaction.options.get("length", true).value as string;
     const reason_id = interaction.options.get("reason", true).value as string;
-
+    
     commandLog(interaction, "/grantmembership", "Green", [
       {
         name: "user",
@@ -66,23 +66,41 @@ export const grantmembership: Command = {
 
     const { contact_id } = contactResponse.data[0];
 
-    const membershipResponse = await insertMembership(
-      { contact_id },
-      length,
-      reason_id
-    );
+    const isAMember = await isMember({contact_id});
+ 
+    if (isAMember.error) {
 
-    if (membershipResponse.error) {
-      await sendError(errorTitle, membershipResponse.message, interaction);
+      const membershipResponse = await insertMembership(
+        { contact_id },
+        length,
+        reason_id
+      );
+
+      if (membershipResponse.error) {
+        await sendError(errorTitle, membershipResponse.message, interaction);
+        return;
+      }
+
+      const returnMessage = createEmbeded(
+        "✅ Membership Granted!",
+        `<@${discord_snowflake}> has successfully received a ${length} long membership!`,
+        client
+      ).setColor("Green");
+      await interaction.editReply({ embeds: [returnMessage] });
       return;
+
     }
 
     const returnMessage = createEmbeded(
-      "✅ Membership Granted!",
-      `<@${discord_snowflake}> has successfully received a ${length} long membership!`,
+      "❌ Membership Denied!",
+      `<@${discord_snowflake}> has already received a membership!`,
       client
-    ).setColor("Green");
+    ).setColor("Red");
     await interaction.editReply({ embeds: [returnMessage] });
     return;
+    
+    
+    
+
   },
 };
