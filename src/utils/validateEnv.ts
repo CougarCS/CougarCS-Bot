19324ebc04b332d35d75221429518b38ec95d233
Ben Key <54153434+mellophone@users.noreply.github.com>
2023-05-10 18:53:47 -0500

    Update validateEnv

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -4,15 +4,22 @@ export const validateEnv = () => {
     return false;
   }
 
-  if (!process.env.GUILD_ID) {
-    console.warn("Missing Guild ID.");
-    return false;
-  }
-
   if (!process.env.CLIENT_ID) {
     console.warn("Missing Creator ID.");
     return false;
   }
+
+  if (!process.env.SUPABASE_URL) {
+    console.warn("Missing Supabase URL.");
+    return false;
+  }
+
+  if (!process.env.SUPABASE_KEY) {
+    console.warn("Missing Supabase Key.");
+    return false;
+  }
+
+  console.log(`Supabase URL: ${process.env.SUPABASE_URL}`);
 
   return true;
 };
