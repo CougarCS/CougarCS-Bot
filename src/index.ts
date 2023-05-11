a32bb0181fd7dbdf99e7fab0b37880401aea36a2
Ben Key <54153434+mellophone@users.noreply.github.com>
2023-05-10 19:03:48 -0500

    Log test

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,14 @@ import { IntentOptions } from "./config/intentOptions";
 import "dotenv/config";
 
 (async () => {
-  if (!validateEnv()) return;
-  const client = new Client({ intents: IntentOptions });
-
-  client.on("ready", async () => await onReady(client));
-
-  client.on(
-    "interactionCreate",
-    async (interaction) => await onInteraction(interaction, client)
-  );
-
-  await client.login(process.env.TOKEN);
+  console.log("Test");
+  console.log(`Supabase URL: ${process.env.SUPABASE_URL}`);
+  // if (!validateEnv()) return;
+  // const client = new Client({ intents: IntentOptions });
+  // client.on("ready", async () => await onReady(client));
+  // client.on(
+  //   "interactionCreate",
+  //   async (interaction) => await onInteraction(interaction, client)
+  // );
+  // await client.login(process.env.TOKEN);
 })();
