76a55329c21ffe4aadb2787c80f55f7e8a710829
Ben Key <54153434+mellophone@users.noreply.github.com>
2023-07-09 21:07:54 -0500

    Update to new Discord.js version

--- src/utils/heartbeat.ts
+++ src/utils/heartbeat.ts
@@ -1,25 +1,28 @@
 import fetch from "node-fetch";
 
 const heartbeat = () => {
-  setInterval(async () => {
-    try {
-      const res = await fetch(process.env.HEARTBEAT_URL as string, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          status: "alive",
-        }),
-      });
+  const url = process.env.HEARTBEAT_URL as string;
 
-      if (res.status !== 200) {
-        return;
+  url !== "https://localhost=9000" &&
+    setInterval(async () => {
+      try {
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            status: "alive",
+          }),
+        });
+
+        if (res.status !== 200) {
+          return;
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  }, 2 * 60 * 1000);
+    }, 2 * 60 * 1000);
 };
 
 export default heartbeat;
