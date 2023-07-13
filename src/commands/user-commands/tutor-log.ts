ce7ed214defedeaf70c38136e2fc8b548960ff15
Joseph Jophy <79068629+josephjophy@users.noreply.github.com>
2023-07-13 18:09:31 -0500

    Fix type error

---
+++ src/commands/user-commands/tutor-log.ts
@@ -0,0 +1,63 @@
+import { Command } from "src/interfaces/Command";
+import { commandLog } from "../../utils/logs";
+import { SlashCommandBuilder } from "discord.js";
+import { reportOptions } from "src/utils/options";
+import { TutorLogInsert } from "src/utils/types";
+import { insertTutorLog } from "src/utils/supabase";
+
+
+
+export const tutorlog: Command = {
+    data : new  SlashCommandBuilder()
+    .setName("tutor-log")
+    .addNumberOption((option) =>
+      option
+        .setName("hours")
+        .setDescription("How many hours did you tutor?")
+        .setMaxValue(5)
+        .setMinValue(1)
+        .setRequired(false)
+    )
+    .addStringOption((option) =>
+    option
+      .setName("tutoring-type")
+      .setDescription("Online or in-person?")
+      .addChoices(
+        {name: 'In Person', value: 't-ip'},
+        {name: 'Online Voice Chat', value: 't-ov'},
+        {name: 'Online Text Chat', value: 't-ot'}
+        )
+      .setRequired(true)
+  )
+    .addStringOption((option) =>
+    option
+        .setName("tutored_user")
+        .setDescription("Name of the student you tutored")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+    option
+    .setName("description")
+    .setDescription("Optional description of the tutoring session")
+    .setChoices(...reportOptions())
+    .setRequired(false)
+    )
+  ,
+    run: async (interaction) => {
+        await interaction.deferReply({ ephemeral: false });
+        
+        const create: TutorLogInsert = {
+            hours: interaction.options.get("hours", true).value as number,
+            tutoring_type_id: interaction.options.get("tutoring-type", true).value as string,
+            tutored_user: interaction.options.get("tutored-user", true).value as string,
+            description: interaction.options.get("description", true).value as string,
+        }
+        commandLog(interaction, "/tutor-log", "Orange");
+
+        const tutorLogResponse = {}
+        
+        */
+    }
+    
+
+};
