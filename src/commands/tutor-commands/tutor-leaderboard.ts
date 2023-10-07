352544b8ed8de9fa6c4dc9cb063fb4d8cde2291f
programwithjohn <john.cox0321@gmail.com>
2023-10-07 13:05:41 -0500

    feat(tutor-leaderboard): Create tutor-leaderboard command file with code

---
+++ src/commands/tutor-commands/tutor-leaderboard.ts
@@ -0,0 +1,54 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../../interfaces/Command";
+import { createEmbed } from "../../utils/embeded";
+import { getTutorLeaderboard } from "../../utils/supabase";
+import { commandLog, sendError } from "../../utils/logs";
+
+const tutorleaderboardBody = (array: string[]): string => {
+  let body = "";
+  array.forEach((slot) => (body = `${body}${slot}\n`));
+  return body;
+};
+
+export const tutorleaderboard: Command = {
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("See the CougarCoin leaderboard!")
+    .addNumberOption((option) =>
+      option
+        .setName("number")
+        .setDescription("Number of leaderboard spaces you want to see!")
+        .setMaxValue(20)
+        .setMinValue(1)
+        .setRequired(false)
+    ),
+  run: async (interaction) => {
+    await interaction.deferReply({ ephemeral: false });
+    const number = interaction.options.get("number", false);
+    commandLog(interaction, "/leaderboard", "Green", [
+      {
+        name: "number",
+        value: `${number}`,
+      },
+    ]);
+
+    const errorTitle = "❌ Leaderboard Canceled!";
+
+    const maxSlots = (number?.value || 10) as number;
+    const leaderboardResponse = await getTutorLeaderboard(maxSlots);
+
+    if (leaderboardResponse.error) {
+      await sendError(errorTitle, leaderboardResponse.message, interaction);
+      return;
+    }
+
+    const leaderboardString = tutorleaderboardBody(leaderboardResponse.data);
+
+    const returnMessage = createEmbed(
+      "<a:CC:991512220909445150> CougarCoin Leaderboard!",
+      leaderboardString || "The leaderboard is empty!"
+    ).setColor("Green");
+    await interaction.editReply({ embeds: [returnMessage] });
+    return;
+  },
+};
